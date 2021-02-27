8ddef17fbac7ef7791bb39ee2136756c2fb1af08
Victor Persa <victor.persa@fanduel.com>
2021-02-27 18:45:23 +0200

    public 2

--- Sources/SwiftPackage/Maths.swift
+++ Sources/SwiftPackage/Maths.swift
@@ -1,5 +1,5 @@
 public struct Maths {
-    static func add(numbers: Int...) -> Int {
+    public static func add(numbers: Int...) -> Int {
         var sum = 0
         numbers.forEach { number in
             sum += number
