17d7b4ce31834f6f31a93b4349bd30bd1575a3fd
Victor Persa <victor.persa@gmail.com>
2021-02-28 15:15:29 +0200

    Update swift.yml (#2)
    Changing swift action to run on PR only
    Changing build job to run on ubuntu

--- Tests/SwiftPackageTests/XCTestManifests.swift
+++ Tests/SwiftPackageTests/XCTestManifests.swift
@@ -4,7 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(SwiftPackageTests.allTests),
-        testCase(MathTests.allTests)
+        testCase(MathsTests.allTests)
     ]
 }
 #endif
